4600e47a1387cd45a19b70304fb8196d69276dd2
william <whkz10140@gmail.com>
2017-12-21 17:40:03 +0800

    update global config

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = {
+    global: {
+        $: true,
+        wx: true,
+        template: true,
+    },
     env: {
         browser: true,
         node: true,
@@ -6,7 +11,9 @@ module.exports = {
     },
     rules: {
         indent: [2, 4],
-        'import/extensions': 1,
+        'import/extensions': 0,
+        'import/prefer-default-export': 1,
+        'import/no-extraneous-dependencies': 0,
         'no-unused-expressions': [
             2,
             {
@@ -17,26 +24,22 @@ module.exports = {
         'no-param-reassign': 1,
         'no-plusplus': 1,
         'no-restricted-globals': 1,
-
         'react/jsx-indent-props': [2, 4],
         'react/jsx-indent': [2, 4],
         'react/jsx-filename-extension': 0,
         'react/forbid-prop-types': 1,
         'react/prefer-stateless-function': 1,
         'react/no-array-index-key': 1,
-
         'jsx-a11y/label-has-for': 1,
         'jsx-a11y/click-events-have-key-events': 1,
         'jsx-a11y/anchor-is-valid': [
-            'error',
+            1,
             {
                 components: ['Link'],
                 specialLink: ['hrefLeft', 'hrefRight', 'to'],
                 aspects: ['noHref', 'invalidHref', 'preferButton'],
             },
         ],
-
-        'import/prefer-default-export': 1,
     },
     settings: {
         'import/resolver': {
@@ -44,7 +47,7 @@ module.exports = {
                 paths: ['/usr/local/lib/node_modules'],
             },
             webpack: {
-                config: 'webpack.config.babel.js',
+                config: 'webpack.config.eslint.js',
             },
         },
     },
